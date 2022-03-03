899a2a988472218de40e28c2e0dc8cbcb1e489e4
JedwardMook <jedwardmook@yahoo.com>
2022-03-03 12:03:18 -0600

    commit tree 94c80537e996e933b923760126cfe21bad1d6cda

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
 // Code EyesOnMe Component Here
+
+
+function EyesOnMe(){
+    function handleFocus(){
+        console.log('Good!')
+    }
+
+    function handleBlur(){
+        console.log('Hey! Eyes on me!')
+    }
+
+    return (
+        <div>
+        <button
+        onBlur={handleBlur}
+        onFocus={handleFocus}>'Eyes on me'</button>
+        </div>
+    );
+};
+
+export default EyesOnMe;
